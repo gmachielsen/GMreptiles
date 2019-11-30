e158f766d87105037e599acdc54bf624d8f736a5
Gijs Machielsen <g.machielsen@gmail.com>
2019-11-30 22:52:45 +0100

    little change

--- controllers/product.js
+++ controllers/product.js
@@ -265,3 +265,26 @@ exports.photo = (req, res, next) => {
     }
     next();
 };
+
+exports.listSearch = (req, res) => {
+  //create query object to hold search value and category value
+  const query = {}
+  //assing search value to query.name
+  if(req.query.search) {
+    query.name = {$regex: req.query.search, $options: "i"}
+    //assign category value to query.category
+    if(req.query.category && req.query.category != "All") {
+        query.category = req.query.category
+    }
+    // find the product based on query object with 2 properties
+    // search and category
+    Product.find(query, (err, products) => {
+      if(err) {
+        return res.status(400).json({
+          error: dbErrorHandler(err)
+        })
+      }
+      res.json(products)
+    }).select("-photo")
+  }
+}
