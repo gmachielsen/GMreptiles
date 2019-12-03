90528f74169d441e1a44157b1d71078a396c0232
Gijs Machielsen <g.machielsen@gmail.com>
2019-12-03 15:22:06 +0100

    final commit before presentation

--- controllers/order.js
+++ controllers/order.js
@@ -1,19 +1,21 @@
-const {Order, CartItem} = require("../models/order")
-const { errorHandler } = require("../helpers/dbErrorHandler");
+const { Order, CartItem } = require('../models/order');
+const { errorHandler } = require('../helpers/dbErrorHandler');
+// sendgrid for email npm i @sendgrid/mail
+
 
 exports.orderById = (req, res, next, id) => {
-  Order.findById(id)
-  .populate("products.product", "name price")
-  .exec((err, order) => {
-    if(err || !order) {
-      return res.status(400).json({
-        error: errorHandler(err)
-      });
-    }
-    req.order = order
-    next();
-  })
-}
+    Order.findById(id)
+        .populate('products.product', 'name price')
+        .exec((err, order) => {
+            if (err || !order) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            req.order = order;
+            next();
+        });
+};
 
 exports.create = (req, res) => {
   // console.log("CREATE ORDER:", req.body);
@@ -30,33 +32,30 @@ exports.create = (req, res) => {
 };
 
 exports.listOrders = (req, res) => {
-  Order.find()
-  .populate("user", "_id name address")
-  .sort("-created")
-  .exec((err, orders) => {
-    if(err) {
-      return res.status(400).json({
-        error: errorHandler(error)
-      });
-    }
-    res.json(orders);
-  })
-}
-
+    Order.find()
+        .populate('user', '_id name address')
+        .sort('-created')
+        .exec((err, orders) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(error)
+                });
+            }
+            res.json(orders);
+        });
+};
 
 exports.getStatusValues = (req, res) => {
-  res.json(Order.schema.path("status").enumValues);
+    res.json(Order.schema.path('status').enumValues);
 };
 
 exports.updateOrderStatus = (req, res) => {
-  Order.update({_id: req.body.orderId}, {$set: {status: req.body.status}},
-    (err, order) => {
-      if(err) {
-        return res.status(400).json({
-          error: errorHandler(err)
-        });
-      }
-      res.json(order);
-    }
-  );
+    Order.update({ _id: req.body.orderId }, { $set: { status: req.body.status } }, (err, order) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(order);
+    });
 };
--- models/user.js
+++ models/user.js
@@ -12,54 +12,51 @@ const uuidv1 = require("uuid/v1");
 
 // var uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema = new mongoose.Schema({
-      name: {
-        type: String,
-        trim: true,
-        required: true,
-        maxlength: 32
-      },
-      email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        dropDups: true
-      },
-      hashed_password: {
-        type: String,
-        required: true,
-      },
-      about: {
-        type: String,
-        trim: true,
-      },
-      salt: String,
-      role: {
-        type: Number,
-        default: 0
-      },
-      history: {
-        type: Array,
-        default: []
-      }
-  },
-  {timestamps: true }
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true
+        },
+        hashed_password: {
+            type: String,
+            required: true
+        },
+        about: {
+            type: String,
+            trim: true
+        },
+        salt: String,
+        role: {
+            type: Number,
+            default: 0
+        },
+        history: {
+            type: Array,
+            default: []
+        }
+    },
+    { timestamps: true }
 );
 
-// virtualfield
-
+// virtual field
 userSchema
-    .plugin(uniqueValidator)
-    .virtual("password")
+    .virtual('password')
     .set(function(password) {
-      this._password = password;
-      this.salt = uuidv1()
-      this.hashed_password = this.encryptPassword(password);
-    });
-userSchema.virtual("password")
+        this._password = password;
+        this.salt = uuidv1();
+        this.hashed_password = this.encryptPassword(password);
+    })
     .get(function() {
-        return this._password
+        return this._password;
     });
 
 userSchema.methods = {
@@ -68,16 +65,16 @@ userSchema.methods = {
     },
 
     encryptPassword: function(password) {
-        if(!password) return "";
+        if (!password) return '';
         try {
-          return crypto
-              .createHmac("sha1", this.salt)
-              .update(password)
-              .digest("hex")
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
         } catch (err) {
-            return "";
+            return '';
         }
     }
 };
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model('User', userSchema);
